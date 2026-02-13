5bd350d4cbe74a36c6b8def8027a62e0d56804a0
q <MatheenatSong@gmail.com>
2026-02-13 18:16:40 +0700

    init class player

--- player.py
+++ player.py
@@ -1,2 +1,17 @@
 import pygame # type: ignore
-import ui
+import math
+from typing import TypedDict
+import constant_value # type: ignore
+from sprites import sprite_loader # type: ignore
+class player_config(TypedDict):
+        Max_HP : int
+        Speed : int
+        Sprite : str
+        
+class Player(pygame.sprite.Sprite):
+    def __init__(self, screen: pygame.Surface, sprite_loader, config_data: player_config):
+        super().__init__()
+        self.config = config_data
+        stats = self.config.get("player_settings",{})
+        self.max_hp = stats.get("Max_HP", 100)
+        self.speed = stats.get("Speed", 5)
